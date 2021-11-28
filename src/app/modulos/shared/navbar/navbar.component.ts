48163ed8f729d22e4a6fb1fc87b782be84c7dcaf
Antonio Martin Franco <antoniofranco.dev@gmail.com>
2021-11-28 16:06:22 -0300

    update navbar y styles global

--- src/app/modulos/shared/navbar/navbar.component.ts
+++ src/app/modulos/shared/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { MegaMenuItem, MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-  /* Tada usada de ejemlo */
+export class NavbarComponent implements OnInit {
+  items!: MenuItem[];
+  /* Tada usada de ejemplo */
   userData = {
     name: 'Antonio',
-    img: 'https://thispersondoesnotexist.com/image',
+    img: 'https://i.pinimg.com/564x/46/9b/35/469b3549ff96553e1fa803020bd0c33e.jpg',
   };
+  // TODO: Las rutas van a venir segun el rol del AUTH
+  rutas = [
+    { url: '/Ventas', name: 'Ventas' },
+    { url: '/Deposito', name: 'Deposito' },
+  ];
   constructor() {}
+
+  ngOnInit() {}
 }
